2a82e227e5133b1d688f79ba26e7280c7e9308ba
Joe Yates <joe.g.yates@gmail.com>
2014-01-16 23:33:02 +0100

    Add facts about files/files-of-type

--- test/sloccount/files_test.clj
+++ test/sloccount/files_test.clj
@@ -18,3 +18,11 @@
   (fact "it excludes binary files"
     (files/source-files "path") => ["foo"]
       (provided (files/filenames "path") => ["text.doc" "foo"])))
+
+(facts "about `files-of-type`"
+  (fact "it returns filenames"
+    (files/files-of-type "path" "foo") => ["foo_file"]
+      (provided
+        (files/filenames "path") => ["foo_file", "qux_file"],
+        (sloccount.files/file-type "foo_file") => "foo",
+        (sloccount.files/file-type "qux_file") => "qux")))
